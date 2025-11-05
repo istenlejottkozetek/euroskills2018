fb4ce6ebf0376969390b9a8be0ea022153a3659a
villas.attila <villas.attila@BURG-1E05-09>
2025-11-05 08:22:40 +0100

    Add project files.

---
+++ euroskills/Models/Orszag.cs
@@ -0,0 +1,8 @@
+﻿namespace EuroskillsAPI.Models
+{
+    public class Orszag
+    {
+        public string Id { get; set; } = string.Empty;
+        public string OrszagNev { get; set; } = string.Empty;
+    }
+}
---
+++ euroskills/Models/Szakma.cs
@@ -0,0 +1,8 @@
+﻿namespace EuroskillsAPI.Models
+{
+    public class Szakma
+    {
+        public string Id { get; set; } = string.Empty;
+        public string SzakmaNev { get; set; } = string.Empty;
+    }
+}
